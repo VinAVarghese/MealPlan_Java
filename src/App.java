667eb587c418d40d4c7cd16351f95096b0c0fb40
Vins MacBook <vinavarghese@gmail.com>
2020-11-04 10:21:44 -0500

    Adding notes on arrays and reading inputs and arithmatic

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -6,5 +7,9 @@ public class App {
         System.out.println(message);
         Date now = new Date();
         System.out.println(now);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name A Meal You Like: ");
+        String meal = scanner.nextLine();
+        System.out.println("This is your first meal: " + meal);
     }
 }
